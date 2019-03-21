8c13a79a55c857d58279900241791f231844b48f
jeffrey-wentz <jeffrey.a.wentz@gmail.com>
2019-03-20 18:57:26 -0700

    finished the dice roller version 1

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import random
 import main_functions
 
 while True:
     user_die = main_functions.get_die()
-    print("User die: " + str(user_die))
-
+    main_functions.roll_die(user_die)
     cont = input("Continue?: ")
-    if cont.lower() not in ("no", "n", "0"):
+    if cont.lower() in ("no", "n", "0"):
         break
+    print("")
--- main_functions.py
+++ main_functions.py
@@ -1,8 +1,11 @@
+import random
 
 
+# this function gets how many die are being rolled and how many sides the die have
 def get_die():
     die_num = 0
     die_type = 0
+    # while loop ensures the user enters a minimum of 1 for the number of die and sides
     while die_type < 1 and die_num < 1:
         try:
             die_num = int(input("Please enter how many die you would like to roll: "))
@@ -14,7 +17,18 @@ def get_die():
                 if die_type < 1:
                     print("Die type must be greater than 0!")
                     die_type = 0
-            if die_num >= 1 and die_type >= 1:
-                return die_num, die_num
+            # returns the values once the user has entered valid numbers for the input
+            if die_num > 0 and die_type > 0:
+                return die_num, die_type
         except ValueError:
             print("Must enter a number!")
+
+
+# this function stores the dice rolls in an empty list then prints the results in a beautiful manner
+def roll_die(user_die):
+    die_results = []
+    # appends a dice roll to die_results as many times as the user entered rolls
+    for num in range(0, user_die[0]):
+        die_results.append(random.randint(1, user_die[1]))
+    print("Results: " + str(die_results))
+    print("Sum total: " + str(sum(die_results)) + "\n")
